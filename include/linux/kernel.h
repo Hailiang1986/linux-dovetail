b7470f74f4b868467ef04a6a3f56e55a6da86f5e
Philippe Gerum <rpm@xenomai.org>
2016-07-20 15:06:15 +0200

    genirq: irq_pipeline: add IRQ pipeline core
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    
    genirq: irq_pipeline: fix irq flow handling with stacked irqchips
    
    When irqchips are stacked, a cascaded event decoded by a parent irq
    which is not delivered oob won't receive any ack/eoi, which is
    wrong. Failing to ack any unmasked edge event could quickly yield an
    interrupt storm. E.g. on x86, with a gpio irq controller stacked on
    top of the IO/APIC:
    
    handle_irq_pipelined_prepare()
    [on_pipeline_entry: true]
    arch_handle_irq()
            handle_fasteoi_irq()
                    handle_oob_irq()
                            mask_cond_eoi_irq()
    handle_irq_pipelined_finish()
    [on_pipeline_entry: false]
            synchronize_pipeline_on_irq()
                    handle_fasteoi_irq() [!on_pipeline_entry]
                          intel_gpio_irq()
                               handle_edge_irq() <<< failed to ack
    
    To solve this, we need to track the deferral status of every irq
    event, running the latter through the pipeline injection code when
    required, regardless of the current stage.
    
    See https://lore.kernel.org/xenomai/MN2PR11MB42855A234CF8BF7D17E9E2ECF2E6A@MN2PR11MB4285.namprd11.prod.outlook.com/.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    Signed-off-by: Hongzhan Chen <hongzhan.chen@intel.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -34,6 +34,7 @@
 #include <linux/static_call_types.h>
 #include <linux/instruction_pointer.h>
 #include <asm/byteorder.h>
+#include <asm-generic/irq_pipeline.h>
 
 #include <uapi/linux/kernel.h>
 
@@ -114,7 +115,7 @@ extern int dynamic_might_resched(void);
 
 #else
 
-# define might_resched() do { } while (0)
+# define might_resched() check_inband_stage()
 
 #endif /* CONFIG_PREEMPT_* */
 
