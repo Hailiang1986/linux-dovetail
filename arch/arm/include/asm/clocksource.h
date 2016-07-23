0b43e9155ae5e9854879e43736c119b4825337e1
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2016-07-23 14:36:14 +0200

    ARM: VDSO: use mmio clocksources

--- arch/arm/include/asm/clocksource.h
+++ arch/arm/include/asm/clocksource.h
@@ -9,9 +9,29 @@ enum vdso_arch_clockmode {
 	VDSO_CLOCKMODE_ARCHTIMER_NOCOMPAT = VDSO_CLOCKMODE_ARCHTIMER,
 };
 
+enum arch_clock_uaccess_type {
+	ARM_CLOCK_NONE = 0,
+	ARM_CLOCK_ARCH_TIMER,
+
+	ARM_CLOCK_USER_MMIO_BASE, /* Must remain last */
+};
+
 struct arch_clocksource_data {
 	/* Usable for direct VDSO access? */
 	enum vdso_arch_clockmode clock_mode;
+	enum arch_clock_uaccess_type clock_type;
 };
+
+#ifdef CONFIG_VDSO
+
+#define arch_clocksource_arch_timer_init \
+	arch_clocksource_arch_timer_init
+void arch_clocksource_arch_timer_init(struct clocksource *cs);
+
+#define arch_clocksource_user_mmio_init \
+	arch_clocksource_user_mmio_init
+void arch_clocksource_user_mmio_init(struct clocksource *cs, unsigned id);
+
+#endif /* CONFIG_VDSO */
 
 #endif
