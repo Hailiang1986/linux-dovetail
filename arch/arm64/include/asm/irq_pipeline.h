cerna/linux-evl

ARM64: irq_pipeline: add IRQ pipeline core

arch/arm64/include/asm/irq_pipeline.h
@@ -1,0 +1,137 @@
+/*
+ * arch/arm64/include/asm/irq_pipeline.h
+ *
+ * Copyright (C) 2002-2017 Philippe Gerum <rpm@xenomai.org>.
+ * Copyright (C) 2005 Stelian Pop.
+ * Copyright (C) 2006-2008 Gilles Chanteperdrix.
+ * Copyright (C) 2010 Philippe Gerum (SMP port).
+ */
+#ifndef _ASM_ARM64_IRQ_PIPELINE_H
+#define _ASM_ARM64_IRQ_PIPELINE_H
+
+#define IPIPE_NR_XIRQS		3072
+
+#include <asm-generic/irq_pipeline.h>
+
+#ifdef CONFIG_IRQ_PIPELINE
+
+#define IPIPE_CORE_RELEASE	1
+
+#define IPIPE_IPI_BASE		2048
+#define IPIPE_CRITICAL_IPI	(IPIPE_IPI_BASE + IPI_PIPELINE_CRITICAL)
+#define IPIPE_HRTIMER_IPI	(IPIPE_IPI_BASE + IPI_PIPELINE_HRTIMER)
+#define IPIPE_RESCHEDULE_IPI	(IPIPE_IPI_BASE + IPI_PIPELINE_RESCHEDULE)
+
+static inline notrace
+unsigned long arch_irqs_virtual_to_native_flags(int stalled)
+{
+	return (!!stalled) << IRQMASK_I_POS;
+}
+
+static inline notrace
+unsigned long arch_irqs_merge_flags(unsigned long flags, int stalled)
+{
+	flags <<= IRQMASK_I_SHIFT;
+	flags &= IRQMASK_I_BIT << IRQMASK_I_SHIFT;
+	return flags | arch_irqs_virtual_to_native_flags(stalled);
+}
+
+static inline notrace
+unsigned long arch_irqs_split_flags(unsigned long flags, int *stalled)
+{
+	*stalled = !!(flags & IRQMASK_I_BIT);
+	flags &= IRQMASK_I_BIT << IRQMASK_I_SHIFT;
+	return flags >> IRQMASK_I_SHIFT;
+}
+
+static inline notrace unsigned long arch_local_irq_save(void)
+{
+	int stalled = root_irq_save();
+	barrier();
+	return arch_irqs_virtual_to_native_flags(stalled);
+}
+
+static inline notrace void arch_local_irq_enable(void)
+{
+	barrier();
+	root_irq_enable();
+}
+
+static inline notrace void arch_local_irq_disable(void)
+{
+	root_irq_disable();
+	barrier();
+}
+
+static inline notrace unsigned long arch_local_save_flags(void)
+{
+	int stalled = root_irqs_disabled();
+	barrier();
+	return arch_irqs_virtual_to_native_flags(stalled);
+}
+
+static inline int arch_irqs_disabled_flags(unsigned long flags)
+{
+	return native_irqs_disabled_flags(flags);
+}
+
+static inline notrace void arch_local_irq_restore(unsigned long flags)
+{
+	if (!arch_irqs_disabled_flags(flags))
+		__root_irq_enable();
+	barrier();
+}
+
+static inline
+void arch_save_timer_regs(struct pt_regs *dst,
+			  struct pt_regs *src, bool head_context)
+{
+	dst->pstate = src->pstate;
+	dst->pc = src->pc;
+	if (head_context)
+		dst->pstate |= IRQMASK_I_BIT;
+}
+
+static inline bool arch_steal_pipelined_tick(struct pt_regs *regs)
+{
+	return !!(regs->pstate & IRQMASK_I_BIT);
+}
+
+struct irq_stage;
+static inline void arch_irq_push_stage(struct irq_stage *stage) { }
+
+#else  /* !CONFIG_IRQ_PIPELINE */
+
+static inline unsigned long arch_local_irq_save(void)
+{
+	return native_irq_save();
+}
+
+static inline void arch_local_irq_enable(void)
+{
+	native_irq_enable();
+}
+
+static inline void arch_local_irq_disable(void)
+{
+	native_irq_disable();
+}
+
+static inline unsigned long arch_local_save_flags(void)
+{
+	return native_save_flags();
+}
+
+static inline void arch_local_irq_restore(unsigned long flags)
+{
+	native_irq_restore(flags);
+}
+
+static inline int arch_irqs_disabled_flags(unsigned long flags)
+{
+	return native_irqs_disabled_flags(flags);
+}
+
+#endif /* !CONFIG_IRQ_PIPELINE */
+
+#endif /* _ASM_ARM64_IRQ_PIPELINE_H */

arch/arm64/kernel/irq_pipeline.c
@@ -1,0 +1,103 @@
+/* -*- linux-c -*-
+ * arch/arm64/kernel/irq_pipeline.c
+ *
+ * Copyright (C) 2002-2017 Philippe Gerum <rpm@xenomai.org>.
+ * Copyright (C) 2004 Wolfgang Grandegger (Adeos/arm port over 2.4).
+ * Copyright (C) 2005 Heikki Lindholm (PowerPC 970 fixes).
+ * Copyright (C) 2005 Stelian Pop.
+ * Copyright (C) 2006-2008 Gilles Chanteperdrix.
+ * Copyright (C) 2010 Philippe Gerum (SMP port).
+ */
+#include <linux/kernel.h>
+#include <linux/smp.h>
+#include <linux/irq.h>
+#include <linux/irq_pipeline.h>
+
+#ifdef CONFIG_SMP
+
+static struct irq_domain *sipic_domain;
+
+static void sipic_irq_noop(struct irq_data *data) { }
+
+static unsigned int sipic_irq_noop_ret(struct irq_data *data)
+{
+	return 0;
+}
+
+static struct irq_chip sipic_chip = {
+	.name		= "SIPIC",
+	.irq_startup	= sipic_irq_noop_ret,
+	.irq_shutdown	= sipic_irq_noop,
+	.irq_enable	= sipic_irq_noop,
+	.irq_disable	= sipic_irq_noop,
+	.irq_ack	= sipic_irq_noop,
+	.irq_mask	= sipic_irq_noop,
+	.irq_unmask	= sipic_irq_noop,
+	.flags		= IRQCHIP_PIPELINE_SAFE | IRQCHIP_SKIP_SET_WAKE,
+};
+
+static int sipic_irq_map(struct irq_domain *d, unsigned int irq,
+			irq_hw_number_t hwirq)
+{
+	irq_set_percpu_devid(irq);
+	irq_set_chip_and_handler(irq, &sipic_chip, handle_synthetic_irq);
+
+	return 0;
+}
+
+static struct irq_domain_ops sipic_domain_ops = {
+	.map	= sipic_irq_map,
+};
+
+static void create_ipi_domain(void)
+{
+	/*
+	 * Create an IRQ domain for mapping all IPIs, with fixed sirq
+	 * numbers starting from IPIPE_IPI_BASE onward. The sirqs
+	 * obtained can be injected into the pipeline upon IPI receipt
+	 * like other interrupts.
+	 */
+	sipic_domain = irq_domain_add_simple(NULL, NR_IPI, IPIPE_IPI_BASE,
+					     &sipic_domain_ops, NULL);
+}
+
+void irq_pipeline_send_remote(unsigned int ipi,
+			      const struct cpumask *cpumask)
+{
+	enum ipi_msg_type msg = ipi - IPIPE_IPI_BASE;
+	smp_cross_call(cpumask, msg);
+}
+EXPORT_SYMBOL_GPL(irq_pipeline_send_remote);
+
+#endif	/* CONFIG_SMP */
+
+void __init arch_irq_pipeline_init(void)
+{
+#ifdef CONFIG_SMP
+	create_ipi_domain();
+#endif
+}
+
+asmlinkage int __ipipe_check_root_interruptible(void)
+{
+	return on_root_stage() && !irqs_disabled();
+}
+
+void do_IRQ_pipelined(struct irq_desc *desc)
+{
+	struct pt_regs *regs = raw_cpu_ptr(&irq_pipeline.tick_regs);
+	unsigned int irq = irq_desc_get_irq(desc);
+
+#ifdef CONFIG_SMP
+	/*
+	 * Check for IPIs, handing them over to the specific dispatch
+	 * code.
+	 */
+	if (irq >= IPIPE_IPI_BASE && irq < IPIPE_IPI_BASE + NR_IPI) {
+		__handle_IPI(irq - IPIPE_IPI_BASE, regs);
+		return;
+	}
+#endif
+		
+	do_domain_irq(irq, regs);
+}

Hailiang1986/linux-dovetail

arch/arm64/include/asm/irq_pipeline.h
@@ -1,0 +1,133 @@
+/*
+ * arch/arm64/include/asm/irq_pipeline.h
+ *
+ * Copyright (C) 2002-2017 Philippe Gerum <rpm@xenomai.org>.
+ * Copyright (C) 2005 Stelian Pop.
+ * Copyright (C) 2006-2008 Gilles Chanteperdrix.
+ * Copyright (C) 2010 Philippe Gerum (SMP port).
+ */
+#ifndef _ASM_ARM64_IRQ_PIPELINE_H
+#define _ASM_ARM64_IRQ_PIPELINE_H
+
+#include <asm-generic/irq_pipeline.h>
+
+#ifdef CONFIG_IRQ_PIPELINE
+
+#define OOB_IPI_BASE		2048
+#define STOP_OOB_IPI		(OOB_IPI_BASE + IPI_PIPELINE_STOP)
+#define TIMER_OOB_IPI		(OOB_IPI_BASE + IPI_PIPELINE_TIMER)
+#define RESCHEDULE_OOB_IPI	(OOB_IPI_BASE + IPI_PIPELINE_RESCHEDULE)
+
+static inline notrace
+unsigned long arch_irqs_virtual_to_native_flags(int stalled)
+{
+	return (!!stalled) << IRQMASK_I_POS;
+}
+
+static inline notrace
+unsigned long arch_irqs_merge_flags(unsigned long flags, int stalled)
+{
+	flags <<= IRQMASK_I_SHIFT;
+	flags &= IRQMASK_I_BIT << IRQMASK_I_SHIFT;
+	return flags | arch_irqs_virtual_to_native_flags(stalled);
+}
+
+static inline notrace
+unsigned long arch_irqs_split_flags(unsigned long flags, int *stalled)
+{
+	*stalled = !!(flags & IRQMASK_I_BIT);
+	flags &= IRQMASK_I_BIT << IRQMASK_I_SHIFT;
+	return flags >> IRQMASK_I_SHIFT;
+}
+
+static inline notrace unsigned long arch_local_irq_save(void)
+{
+	int stalled = root_irq_save();
+	barrier();
+	return arch_irqs_virtual_to_native_flags(stalled);
+}
+
+static inline notrace void arch_local_irq_enable(void)
+{
+	barrier();
+	root_irq_enable();
+}
+
+static inline notrace void arch_local_irq_disable(void)
+{
+	root_irq_disable();
+	barrier();
+}
+
+static inline notrace unsigned long arch_local_save_flags(void)
+{
+	int stalled = root_irqs_disabled();
+	barrier();
+	return arch_irqs_virtual_to_native_flags(stalled);
+}
+
+static inline int arch_irqs_disabled_flags(unsigned long flags)
+{
+	return native_irqs_disabled_flags(flags);
+}
+
+static inline notrace void arch_local_irq_restore(unsigned long flags)
+{
+	if (!arch_irqs_disabled_flags(flags))
+		__root_irq_enable();
+	barrier();
+}
+
+static inline
+void arch_save_timer_regs(struct pt_regs *dst,
+			  struct pt_regs *src, bool head_context)
+{
+	dst->pstate = src->pstate;
+	dst->pc = src->pc;
+	if (head_context)
+		dst->pstate |= IRQMASK_I_BIT;
+}
+
+static inline bool arch_steal_pipelined_tick(struct pt_regs *regs)
+{
+	return !!(regs->pstate & IRQMASK_I_BIT);
+}
+
+struct irq_stage;
+static inline void arch_irq_push_stage(struct irq_stage *stage) { }
+
+#else  /* !CONFIG_IRQ_PIPELINE */
+
+static inline unsigned long arch_local_irq_save(void)
+{
+	return native_irq_save();
+}
+
+static inline void arch_local_irq_enable(void)
+{
+	native_irq_enable();
+}
+
+static inline void arch_local_irq_disable(void)
+{
+	native_irq_disable();
+}
+
+static inline unsigned long arch_local_save_flags(void)
+{
+	return native_save_flags();
+}
+
+static inline void arch_local_irq_restore(unsigned long flags)
+{
+	native_irq_restore(flags);
+}
+
+static inline int arch_irqs_disabled_flags(unsigned long flags)
+{
+	return native_irqs_disabled_flags(flags);
+}
+
+#endif /* !CONFIG_IRQ_PIPELINE */
+
+#endif /* _ASM_ARM64_IRQ_PIPELINE_H */

arch/arm64/kernel/irq_pipeline.c
@@ -1,0 +1,103 @@
+/* -*- linux-c -*-
+ * arch/arm64/kernel/irq_pipeline.c
+ *
+ * Copyright (C) 2002-2017 Philippe Gerum <rpm@xenomai.org>.
+ * Copyright (C) 2004 Wolfgang Grandegger (Adeos/arm port over 2.4).
+ * Copyright (C) 2005 Heikki Lindholm (PowerPC 970 fixes).
+ * Copyright (C) 2005 Stelian Pop.
+ * Copyright (C) 2006-2008 Gilles Chanteperdrix.
+ * Copyright (C) 2010 Philippe Gerum (SMP port).
+ */
+#include <linux/kernel.h>
+#include <linux/smp.h>
+#include <linux/irq.h>
+#include <linux/irq_pipeline.h>
+
+#ifdef CONFIG_SMP
+
+static struct irq_domain *sipic_domain;
+
+static void sipic_irq_noop(struct irq_data *data) { }
+
+static unsigned int sipic_irq_noop_ret(struct irq_data *data)
+{
+	return 0;
+}
+
+static struct irq_chip sipic_chip = {
+	.name		= "SIPIC",
+	.irq_startup	= sipic_irq_noop_ret,
+	.irq_shutdown	= sipic_irq_noop,
+	.irq_enable	= sipic_irq_noop,
+	.irq_disable	= sipic_irq_noop,
+	.irq_ack	= sipic_irq_noop,
+	.irq_mask	= sipic_irq_noop,
+	.irq_unmask	= sipic_irq_noop,
+	.flags		= IRQCHIP_PIPELINE_SAFE | IRQCHIP_SKIP_SET_WAKE,
+};
+
+static int sipic_irq_map(struct irq_domain *d, unsigned int irq,
+			irq_hw_number_t hwirq)
+{
+	irq_set_percpu_devid(irq);
+	irq_set_chip_and_handler(irq, &sipic_chip, handle_synthetic_irq);
+
+	return 0;
+}
+
+static struct irq_domain_ops sipic_domain_ops = {
+	.map	= sipic_irq_map,
+};
+
+static void create_ipi_domain(void)
+{
+	/*
+	 * Create an IRQ domain for mapping all IPIs, with fixed sirq
+	 * numbers starting from OOB_IPI_BASE onward. The sirqs
+	 * obtained can be injected into the pipeline upon IPI receipt
+	 * like other interrupts.
+	 */
+	sipic_domain = irq_domain_add_simple(NULL, NR_IPI, OOB_IPI_BASE,
+					     &sipic_domain_ops, NULL);
+}
+
+void irq_pipeline_send_remote(unsigned int ipi,
+			      const struct cpumask *cpumask)
+{
+	enum ipi_msg_type msg = ipi - OOB_IPI_BASE;
+	smp_cross_call(cpumask, msg);
+}
+EXPORT_SYMBOL_GPL(irq_pipeline_send_remote);
+
+#endif	/* CONFIG_SMP */
+
+void __init arch_irq_pipeline_init(void)
+{
+#ifdef CONFIG_SMP
+	create_ipi_domain();
+#endif
+}
+
+asmlinkage int __ipipe_check_root_interruptible(void)
+{
+	return on_root_stage() && !irqs_disabled();
+}
+
+void do_IRQ_pipelined(struct irq_desc *desc)
+{
+	struct pt_regs *regs = raw_cpu_ptr(&irq_pipeline.tick_regs);
+	unsigned int irq = irq_desc_get_irq(desc);
+
+#ifdef CONFIG_SMP
+	/*
+	 * Check for IPIs, handing them over to the specific dispatch
+	 * code.
+	 */
+	if (irq >= OOB_IPI_BASE && irq < OOB_IPI_BASE + NR_IPI) {
+		__handle_IPI(irq - OOB_IPI_BASE, regs);
+		return;
+	}
+#endif
+		
+	do_domain_irq(irq, regs);
+}