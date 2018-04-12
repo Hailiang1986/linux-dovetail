cerna/linux-evl

ARM: dovetail: route syscalls and traps to co-kernel

arch/arm/include/asm/dovetail.h
@@ -6,6 +6,19 @@
 #ifndef _ASM_ARM_DOVETAIL_H
 #define _ASM_ARM_DOVETAIL_H
 
+/* ARM traps */
+#define IPIPE_TRAP_ACCESS	 0	/* Data or instruction access exception */
+#define IPIPE_TRAP_SECTION	 1	/* Section fault */
+#define IPIPE_TRAP_DABT		 2	/* Generic data abort */
+#define IPIPE_TRAP_UNKNOWN	 3	/* Unknown exception */
+#define IPIPE_TRAP_BREAK	 4	/* Instruction breakpoint */
+#define IPIPE_TRAP_FPU		 5	/* Floating point exception */
+#define IPIPE_TRAP_VFP		 6	/* VFP floating point exception */
+#define IPIPE_TRAP_UNDEFINSTR	 7	/* Undefined instruction */
+#define IPIPE_TRAP_ALIGNMENT	 8	/* Unaligned access exception */
+#define IPIPE_TRAP_MAYDAY        9	/* Internal recovery trap */
+#define IPIPE_NR_FAULTS         10
+
 #define dovetail_get_active_mm()	__this_cpu_read(irq_pipeline.active_mm)
 
 #define IPIPE_TSC_TYPE_NONE	   		0

arch/arm/kernel/ptrace.c
@@ -214,6 +214,7 @@ void ptrace_break(struct task_struct *tsk, struct pt_regs *regs)
 
 static int break_trap(struct pt_regs *regs, unsigned int instr)
 {
+	dovetail_handle_trap(IPIPE_TRAP_BREAK, regs);
 	ptrace_break(current, regs);
 	return 0;
 }

arch/arm/kernel/traps.c
@@ -514,6 +514,8 @@ asmlinkage void __exception_irq_entry handle_fiq_as_nmi(struct pt_regs *regs)
  */
 asmlinkage void bad_mode(struct pt_regs *regs, int reason)
 {
+	dovetail_handle_trap(IPIPE_TRAP_UNKNOWN, regs);
+
 	console_verbose();
 
 	pr_crit("Bad mode in %s handler detected\n", handler[reason]);

arch/arm/mm/alignment.c
@@ -21,6 +21,7 @@
 #include <linux/init.h>
 #include <linux/sched.h>
 #include <linux/uaccess.h>
+#include <linux/dovetail.h>
 
 #include <asm/cp15.h>
 #include <asm/system_info.h>
@@ -781,6 +782,8 @@ do_alignment(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 
 	if (interrupts_enabled(regs))
 		hard_local_irq_enable();
+
+	dovetail_handle_trap(IPIPE_TRAP_ALIGNMENT, regs);
 
 	instrptr = instruction_pointer(regs);
 

Hailiang1986/linux-dovetail

arch/arm/include/asm/dovetail.h
@@ -6,4 +6,16 @@
 #ifndef _ASM_ARM_DOVETAIL_H
 #define _ASM_ARM_DOVETAIL_H
 
+/* ARM traps */
+#define ARM_TRAP_ACCESS		0	/* Data or instruction access exception */
+#define ARM_TRAP_SECTION	1	/* Section fault */
+#define ARM_TRAP_DABT		2	/* Generic data abort */
+#define ARM_TRAP_UNKNOWN	3	/* Unknown exception */
+#define ARM_TRAP_BREAK		4	/* Instruction breakpoint */
+#define ARM_TRAP_FPU		5	/* Floating point exception */
+#define ARM_TRAP_VFP		6	/* VFP floating point exception */
+#define ARM_TRAP_UNDEFINSTR	7	/* Undefined instruction */
+#define ARM_TRAP_ALIGNMENT	8	/* Unaligned access exception */
+#define ARM_TRAP_MAYDAY		9	/* Internal recovery trap */
+
 #endif /* _ASM_ARM_DOVETAIL_H */

arch/arm/kernel/ptrace.c
@@ -215,6 +215,7 @@ void ptrace_break(struct task_struct *tsk, struct pt_regs *regs)
 
 static int break_trap(struct pt_regs *regs, unsigned int instr)
 {
+	dovetail_handle_trap(ARM_TRAP_BREAK, regs);
 	ptrace_break(current, regs);
 	return 0;
 }

arch/arm/kernel/traps.c
@@ -474,6 +474,8 @@ asmlinkage void do_undefinstr(struct pt_regs *regs)
 		return;
 
 die_sig:
+	dovetail_handle_trap(ARM_TRAP_UNDEFINSTR, regs);
+
 #ifdef CONFIG_DEBUG_USER
 	if (user_debug & UDBG_UNDEFINED) {
 		pr_info("%s (%d): undefined instruction: pc=%p\n",
@@ -524,6 +526,8 @@ asmlinkage void __exception_irq_entry handle_fiq_as_nmi(struct pt_regs *regs)
  */
 asmlinkage void bad_mode(struct pt_regs *regs, int reason)
 {
+	dovetail_handle_trap(ARM_TRAP_UNKNOWN, regs);
+
 	console_verbose();
 
 	pr_crit("Bad mode in %s handler detected\n", handler[reason]);

arch/arm/mm/alignment.c
@@ -22,6 +22,7 @@
 #include <linux/init.h>
 #include <linux/sched/signal.h>
 #include <linux/uaccess.h>
+#include <linux/dovetail.h>
 
 #include <asm/cp15.h>
 #include <asm/system_info.h>
@@ -782,6 +783,8 @@ do_alignment(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 
 	if (interrupts_enabled(regs))
 		hard_local_irq_enable();
+
+	dovetail_handle_trap(ARM_TRAP_ALIGNMENT, regs);
 
 	instrptr = instruction_pointer(regs);
 