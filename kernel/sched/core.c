ad35d0503ea742cf87f0c09492824ca442f79027
Philippe Gerum <rpm@xenomai.org>
2019-08-12 12:12:38 +0200

    dovetail: altsched: prevent the compiler from reordering past switch_to()

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8210,6 +8210,7 @@ bool dovetail_context_switch(struct dovetail_altsched_context *out,
 	}
 
 	switch_to(prev, next, last);
+	barrier();
 
 	if (check_hard_irqs_disabled())
 		hard_irqs_disabled();
