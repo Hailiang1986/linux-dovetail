a6d96a319446724530ed64a7353caec604678c1a
Philippe Gerum <rpm@xenomai.org>
2020-03-28 08:55:56 +0100

    ARM: dovetail: convert to generic oob syscall bit
    Define __OOB_SYSCALL_BIT (0x80000000) as the out-of-band syscall
    marker usable across all architectures Dovetail supports, which is not
    in the range of native syscall numbers.
    
    This removes a longstanding ARM-specific legacy which used to define
    oob syscalls as variations of the __ARM_NR_dovetail special system
    call.
    
    Since Dovetail only supports the EABI model, there is no point in
    keeping this peculiarity which was primarily aimed at coping with the
    OABI, which required the syscall number to be encoded in the SWI
    instruction, only leaving 24 bits for this purpose (or even down to 8
    bits in thumb mode). The EABI conveys the syscall in r7 instead, which
    gives us 32 bits for encoding the oob marker.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/arm/include/uapi/asm/dovetail.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _UAPI_ASM_DOVETAIL_H
-#define _UAPI_ASM_DOVETAIL_H
-
-#define __ARM_NR_dovetail	0xf0042
-
-#endif /* _UAPI_ASM_DOVETAIL_H */
