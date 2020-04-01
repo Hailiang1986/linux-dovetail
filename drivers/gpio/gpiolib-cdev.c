2392274f3cc127d1b20321a905b12855b14d2fee
Philippe Gerum <rpm@xenomai.org>
2020-04-01 12:33:47 +0200

    gpiolib: evl: make out-of-band services depend on GPIOLIB_OOB
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- drivers/gpio/gpiolib-cdev.c
+++ drivers/gpio/gpiolib-cdev.c
@@ -60,11 +60,11 @@ struct linehandle_state {
 	GPIOHANDLE_REQUEST_BIAS_DISABLE | \
 	GPIOHANDLE_REQUEST_OPEN_DRAIN | \
 	GPIOHANDLE_REQUEST_OPEN_SOURCE | \
-	(IS_ENABLED(CONFIG_EVL) ? GPIOHANDLE_REQUEST_OOB : 0))
+	(IS_ENABLED(CONFIG_GPIOLIB_OOB) ? GPIOHANDLE_REQUEST_OOB : 0))
 
 static inline bool oob_handling_requested(u32 lflags)
 {
-	return IS_ENABLED(CONFIG_EVL) && lflags & GPIOHANDLE_REQUEST_OOB;
+	return IS_ENABLED(CONFIG_GPIOLIB_OOB) && lflags & GPIOHANDLE_REQUEST_OOB;
 }
 
 static int linehandle_validate_flags(u32 flags)
@@ -237,7 +237,7 @@ static long linehandle_ioctl_compat(struct file *file, unsigned int cmd,
 }
 #endif
 
-#ifdef CONFIG_EVL
+#ifdef CONFIG_GPIOLIB_OOB
 
 static long linehandle_oob_ioctl(struct file *file, unsigned int cmd,
 				unsigned long arg)
@@ -283,7 +283,7 @@ static long linehandle_oob_ioctl(struct file *file, unsigned int cmd,
 	return -EINVAL;
 }
 
-#endif
+#endif	/* CONFIG_GPIOLIB_OOB */
 
 static void linehandle_free(struct linehandle_state *lh)
 {
@@ -313,12 +313,12 @@ static const struct file_operations linehandle_fileops = {
 	.owner = THIS_MODULE,
 	.llseek = noop_llseek,
 	.unlocked_ioctl = linehandle_ioctl,
-#ifdef CONFIG_EVL
+#ifdef CONFIG_GPIOLIB_OOB
 	.oob_ioctl = linehandle_oob_ioctl,
 #endif
 #ifdef CONFIG_COMPAT
 	.compat_ioctl = linehandle_ioctl_compat,
-#ifdef CONFIG_EVL
+#ifdef CONFIG_GPIOLIB_OOB
 	.compat_oob_ioctl = compat_ptr_oob_ioctl,
 #endif
 #endif
@@ -629,7 +629,7 @@ static irqreturn_t lineevent_read_pin(struct lineevent_state *le,
 	return IRQ_HANDLED;
 }
 
-#ifdef CONFIG_EVL
+#ifdef CONFIG_GPIOLIB_OOB
 
 static irqreturn_t lineevent_oob_irq_handler(int irq, void *p)
 {
@@ -733,7 +733,7 @@ static inline int lineevent_init_oob_state(struct lineevent_state *le,
 	return -EINVAL;
 }
 
-#endif	/* !CONFIG_EVL */
+#endif	/* !CONFIG_GPIOLIB_OOB */
 
 static void lineevent_free(struct lineevent_state *le)
 {
@@ -797,7 +797,7 @@ static long lineevent_ioctl_compat(struct file *file, unsigned int cmd,
 static const struct file_operations lineevent_fileops = {
 	.release = lineevent_release,
 	.read = lineevent_read,
-#ifdef CONFIG_EVL
+#ifdef CONFIG_GPIOLIB_OOB
 	.oob_read = lineevent_oob_read,
 	.oob_poll = lineevent_oob_poll,
 #endif
