cerna/linux-evl

x86/hpet: irq_pipeline: enable pipelined clock events

arch/x86/kernel/hpet.c
@@ -418,7 +418,8 @@ static int hpet_legacy_next_event(unsigned long delta,
 static struct clock_event_device hpet_clockevent = {
 	.name			= "hpet",
 	.features		= CLOCK_EVT_FEAT_PERIODIC |
-				  CLOCK_EVT_FEAT_ONESHOT,
+				  CLOCK_EVT_FEAT_ONESHOT |
+				  CLOCK_EVT_FEAT_PIPELINE,
 	.set_state_periodic	= hpet_legacy_set_periodic,
 	.set_state_oneshot	= hpet_legacy_set_oneshot,
 	.set_state_shutdown	= hpet_legacy_shutdown,
@@ -524,7 +525,7 @@ static irqreturn_t hpet_interrupt_handler(int irq, void *data)
 		return IRQ_HANDLED;
 	}
 
-	hevt->event_handler(hevt);
+	clockevents_handle_event(hevt);
 	return IRQ_HANDLED;
 }
 

Hailiang1986/linux-dovetail

Signed-off-by: Philippe Gerum <rpm@xenomai.org>

arch/x86/kernel/hpet.c
@@ -405,7 +405,7 @@ static void hpet_init_clockevent(struct hpet_channel *hc, unsigned int rating)
 	evt->set_next_event	= hpet_clkevt_set_next_event;
 	evt->set_state_shutdown	= hpet_clkevt_set_state_shutdown;
 
-	evt->features = CLOCK_EVT_FEAT_ONESHOT;
+	evt->features = CLOCK_EVT_FEAT_ONESHOT|CLOCK_EVT_FEAT_PIPELINE;
 	if (hc->boot_cfg & HPET_TN_PERIODIC) {
 		evt->features		|= CLOCK_EVT_FEAT_PERIODIC;
 		evt->set_state_periodic	= hpet_clkevt_set_state_periodic;
@@ -518,7 +518,7 @@ static irqreturn_t hpet_msi_interrupt_handler(int irq, void *data)
 		return IRQ_HANDLED;
 	}
 
-	evt->event_handler(evt);
+	clockevents_handle_event(evt);
 	return IRQ_HANDLED;
 }
 