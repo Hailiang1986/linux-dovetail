0ba250cc1b127e954ad257d8054b28816e3a7326
Philippe Gerum <rpm@xenomai.org>
2022-06-19 19:14:43 +0200

    evl/thread: drop useless variable
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/evl/thread.c
+++ kernel/evl/thread.c
@@ -821,7 +821,6 @@ int evl_set_period(struct evl_clock *clock,
 {
 	struct evl_thread *curr = evl_current();
 	unsigned long flags;
-	int ret = 0;
 
 	if (curr == NULL)
 		return -EPERM;
@@ -850,7 +849,7 @@ int evl_set_period(struct evl_clock *clock,
 
 	raw_spin_unlock_irqrestore(&curr->lock, flags);
 
-	return ret;
+	return 0;
 }
 EXPORT_SYMBOL_GPL(evl_set_period);
 
