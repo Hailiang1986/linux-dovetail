43cd98fdf7d9f30408c486cfd6ec7a2f6f691e43
Philippe Gerum <philippe.gerum@exail.com>
2023-10-23 11:48:14 +0200

    net: dovetail: introduce net struct extension
    This is needed to store the per-net information related to oob
    activity.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -37,6 +37,7 @@
 #include <net/netns/smc.h>
 #include <net/netns/bpf.h>
 #include <net/netns/mctp.h>
+#include <net/netoob.h>
 #include <linux/ns_common.h>
 #include <linux/idr.h>
 #include <linux/skbuff.h>
@@ -159,6 +160,8 @@ struct net {
 #endif
 
 	u64			net_cookie; /* written once */
+
+	struct oob_net_state	oob;
 
 #if IS_ENABLED(CONFIG_IP_VS)
 	struct netns_ipvs	*ipvs;
